1979d3d4fa10b54031913e2cdb44dae6cfcf8ae9
Bence Jenei <bencej9812@gmail.com>
2023-02-24 14:06:06 +0100

    Add multiply function

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Here I should create the 4 basic operations present on a calculator
 
-// Return the sum of 2 numbers
+// Returns the sum of 2 numbers
 function add(input1, input2) {
     return input1 + input2;
 }
 
+// Returns the difference of 2 numbers
 function subtract (input1, input2) {
     return input1 - input2;
 }
+
+function multiply (input1, input2) {
+    return input1 * input2;
+}
+console.log(multiply(5, 5));
