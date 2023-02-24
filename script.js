7b199a30d5e12bf179a69e86a54788adf2a0fa15
Bence Jenei <bencej9812@gmail.com>
2023-02-24 14:53:32 +0100

    Create operate function's first prototype

--- script.js
+++ script.js
@@ -10,11 +10,25 @@ function subtract (input1, input2) {
     return input1 - input2;
 }
 
+// Returns the product of 2 numbers
 function multiply (input1, input2) {
     return input1 * input2;
 }
-
+// Returns the quotient of 2 numbers
 function divide (input1, input2) {
     return input1 / input2;
 }
-console.log(divide(17, 5));
+
+function operate (operator, input1, input2) {
+
+    if (operator === "+") {
+        return add(input1, input2);
+    } else if (operator === "-") {
+        return subtract(input1, input2);
+    } else if (operator === "*") {
+        return multiply(input1, input2);
+    } else if (operator === "/") {
+        return divide(input1, input2);
+    }
+}
+console.log(operate("*",5, 5));
